2b28eb39c6d69b82e2d7e16da1fe03ed33f03f21
TOM <89062432+TOM69JK@users.noreply.github.com>
2023-04-30 12:00:05 +0800

    Update rename.js

--- rename.js
+++ rename.js
@@ -1,23 +1,33 @@
-// 66 测试别的 api 入口inte.net  落地ip-api(入口真实城市 运营商 落地真实位置) @Key @奶茶姐
-// Sub-Store脚本操作:例如: https://keywos.cf/name_kkk.js#flag&timeout=1000
-// 参数 timeout=900  ！默认1000单位ms
-const timeout = $arguments["timeout"] ? $arguments["timeout"] : 1000;
-// 参数 flag 参数为加 国旗、运营商:首字母 🅳=电信,🅻=联通,🆈=移动,🆉直连 ！默认不加 
-const flag = $arguments["flag"];
-// 参数 city 则为入口城市，！默认不加参数则是省份
-const citys = $arguments["city"];
-// 参数 batch=  一次检查多少节点 ！默认16
-const batch_size = $arguments["batch"] ? $arguments["batch"] : 16;
+/* 66688
+符号：🅳=电信 🅻=联通 🆈=移动 🆉=直连 🅶=垃圾
+接口：入口查询[inte.net],落地查询[ip-api]；
+功能：根据接口返回的真实结果，重新对节点命名，添加入口城市、落地国家或地区、国内运营商信息；
+作者：@Key @奶茶姐
+用法：Sub-Store脚本操作添加； 只支持 Surge Loon
+地址：https://脚本地址/name.js#flag&timeout=1000
+日期：2023/04/30
+--------
+以下是此脚本支持的参数，多个参数使用"&"连接，参考上述地址为例使用参数。
+[sim]使用简写(第一个字),如: 广移, 而不是: 广东移动 ...
+[timeout=2000ms]最大超时参数，超出允许范围则判定为无效节点，默认1000ms；
+[flag]添加旗帜、运营商符号和直连符号，默认无此参数；
+[city]添加入口城市名，默认不添加城市名，无 city 参数则只输出省份不输出城市；
+[batch=16]每次检查多少节点，默认每次16个节点。
+*/
+
 const $ = $substore
+const sim = $arguments["sim"];
+const flag = $arguments["flag"];
+const citys = $arguments["city"];
 const { isLoon, isSurge, isQX } = $substore.env;
+const batch_size = $arguments["batch"] ? $arguments["batch"] : 16;
+const timeout = $arguments["timeout"] ? $arguments["timeout"] : 1000;
 const target = isLoon ? "Loon" : isSurge ? "Surge" : isQX ? "QX" : undefined;
 async function operator(proxies) {
   const support = (isLoon || isSurge);
-  if (!support) {
-    $.error(`Only supports Loon and Surge!`);
-    return proxies;
-  }
-  const startTime = new Date(); // 获取当前时间作为开始时间
+  if (!support) { $.error(`No Loon or Surge`);
+    return proxies; }
+  const startTime = new Date();
   const prs = proxies.length; //初始节点数
   // console.log("初始节点数 = " + proxies.length);
   let i = 0;
@@ -26,96 +36,59 @@ async function operator(proxies) {
     await Promise.allSettled(
       batch.map(async (proxy) => {
         try {
-          //   const in_info = await queryDNSInfo(proxy.server);
-          //   console.log(proxy.server + "in节点ip = " + JSON.stringify(in_info));
-          //   🅳电信
-          //   🅻联通
-          //   🆈移动
-          //   其他api
           const in_info = await queryDNSInfo(proxy.server);
-
-          const incity = $arguments["city"]
-            ? in_info.data[2].substring(0, 2)
-            : in_info.data[1].substring(0, 2);
-
-          const dly =
-            in_info.data[in_info.data.length - 1] === "电信"
-              ? "🅳"
-              : in_info.data[in_info.data.length - 1] === "联通"
-              ? "🅻"
-              : in_info.data[in_info.data.length - 1] === "移动"
-              ? "🆈"
-              : "";
-          //   let dly = '';
-          //   if (in_info.data[in_info.data.length - 1] === '电信') {
-          //     dly = 'D';
-          //   } else if (in_info.data[in_info.data.length - 1] === '联通') {
-          //     dly = 'L';
-          //   }
-          // console.log("in节点信息🍉" + JSON.stringify(in_info));
-          // 查询出口IP信息
-          const out_info = await queryIpApi(proxy);
-          //   console.log(proxy.server + "out节点信息 = " + JSON.stringify(out_info));
-
-          if (flag) {
+            // console.log(proxy.server + "in节点ip = " + JSON.stringify(in_info));
+            const incity = $arguments["city"]
+            ? (in_info.data[2] || in_info.data[1] || in_info.data[0]).slice(0, 2)
+            : (in_info.data[1] || in_info.data[0]).slice(0, 2);
+            const out_info = await queryIpApi(proxy);
+            if (flag) { const kkEmoji = { '电信': '🅳', '联通': '🅻', '移动': '🆈', };
+              const operator = in_info.data[in_info.data.length - 1];
+              const dly = kkEmoji[operator] || '🅶';
             // emoji
-            if (in_info.ip === out_info.query) {
-              proxy.name =
-                "🆉直连" +
-                "→" +
-                getFlagEmoji(out_info.countryCode) +
-                out_info.country;
+            if (in_info.ip === out_info.query) { 
+                proxy.name = "🆉直连" + "→" + getFlagEmoji(out_info.countryCode) + out_info.country;
             } else {
-              proxy.name =
-                dly +
-                incity +
-                "→" +
-                getFlagEmoji(out_info.countryCode) +
-                out_info.country;
+                proxy.name = dly + incity + "→" + getFlagEmoji(out_info.countryCode) + out_info.country;
+            }
+          } if (sim) {
+          // simple
+            if (in_info.ip === out_info.query) {
+                proxy.name = "直连" + "→" + out_info.country;
+            } else {                
+                proxy.name = incity.slice(0, 1) + (in_info.data[in_info.data.length - 1].length === 2 ? in_info.data[in_info.data.length - 1].slice(0 ,1) : "中转") + "→" + out_info.country;
             }
           } else {
             // no emoji
-            // proxy.name = out_info.country; 只有国家
             if (in_info.ip === out_info.query) {
-              proxy.name =
-                "直连" +
-                "→" +
-                getFlagEmoji(out_info.countryCode) +
-                out_info.country;
-            } else {
-              proxy.name = incity+in_info.data[in_info.data.length - 1] + "→" + out_info.country;
+                proxy.name = "直连" + "→" + out_info.country;
+            } else {                
+                proxy.name = incity + (in_info.data[in_info.data.length - 1].length === 2 ? in_info.data[in_info.data.length - 1] : "中转") + "→" + out_info.country;
+          //console.log(proxy.name)
             }
           }
-
-          // 🇸🇬 广东省 佛山市 移动→新加坡 01
-          //   proxy.name = flag ? getFlagEmoji(out_info.cc) + " " + (in_info === out_info.ip ? out_info.cc : "CN") + "→" + out_info.country : out_info.country;
-
-          // 新增一个去重用字段，该字段不显示在节点名字不需要修改 ,只用于去重, 重复那就是重复节点：入口IP|出口IP
+          // proxy.name = out_info.country; 只有国家
+          // 去重用字段，该字段不显示在节点名字不需要修改 ,只用于去重, 判断方法：入口IP 与 出口IP
           proxy.qc = in_info.ip + "|" + out_info.query;
-          //   console.log(proxy.qc);
-          // proxy.px = out_info.cc;
+        //   console.log(proxy.qc)
         } catch (err) {
           console.log(`err = ${err}`);
-        }
-      })
-    );
-    i += batch_size;
+        } }) ); i += batch_size;
   }
-  // console.log("💰💕去重前的节点信息 = " + JSON.stringify(proxies));
-  // 去除重复的节点
+  // console.log("去重前的节点信息 = " + JSON.stringify(proxies));
   proxies = removeDuplicateName(proxies);
   // console.log("去重后的节点信息 = " + JSON.stringify(proxies));
-  // 加序号
-  const processedProxies = processProxies(proxies);
-  // console.log("排序后的节点信息 = " + JSON.stringify(proxies));
-  // 去除去重时添加的qc属性: ip 与 dns解析ip
+  // 去除去重时添加的qc属性
   proxies = removeqcName(proxies);
-  // console.log("去qc后的节点信息 = " + JSON.stringify(proxies));
+  //console.log("去重后的节点信息 = " + JSON.stringify(proxies));
+  // 按节点全名分组加序号
+  const processedProxies = processProxies(proxies);
+  //console.log("加序号后的节点信息 = " + JSON.stringify(proxies));
   console.log(`初始节点数 = ` + prs);
   console.log(`去重后个数 = ${proxies.length}`);
-  const endTime = new Date(); // 获取当前时间作为结束时间
-  const timeDiff = endTime.getTime() - startTime.getTime(); // 获取时间差（以毫秒为单位）
-  console.log(`方法总耗时 = ${timeDiff / 1000} 秒`); // 将时间差转换为秒并打印到控制台上
+  const endTime = new Date();
+  const timeDiff = endTime.getTime() - startTime.getTime();
+  console.log(`方法总耗时 = ${timeDiff / 1000} 秒`);
   return proxies;
 }
 //入口ip解析
@@ -123,161 +96,41 @@ async function queryDNSInfo(server) {
   return new Promise((resolve) => {
     const ips = server;
     const url = `http://www.inte.net/tool/ip/api.ashx?ip=${server}&datatype=json`;
-    $.http
-      .get({ url })
-      .then((resp) => {
+    $.http.get({ url }).then((resp) => {
         const kkk = JSON.parse(resp.body);
         if (kkk.ip !== "0.0.0.0") {
           resolve(kkk);
-        } else {
-          resolve(ips);
-        }
-      })
-      .catch((err) => {
+        } else {resolve(ips);}
+      }).catch((err) => {
         console.log("dns = " + err);
         reject(err);
       });
   });
 }
-// //baiduapi解析位置 弃
-// //http://opendata.baidu.com/api.php?query=112.34.112.246&co=&resource_id=6006&oe=utf8
-// async function queryDNSInfo(serverip) {
-//     return new Promise((resolve) => {
-//       const ips = serverip;
-//       const url = `http://opendata.baidu.com/api.php?query=${serverip}&co=&resource_id=6006&oe=utf8`;
-//       $.http
-//         .get({ url })
-//         .then((resp) => {
-//           const body = JSON.parse(resp.body);
-//           if (body.length > 0) {
-//             resolve(body[0]);
-//           } else {
-//             resolve(ips);
-//           }
-//         })
-//         .catch((err) => {
-//           console.log("dns = " + err);
-//           reject(err);
-//         });
-//     });
-// //   }
-// // 查询落地ip 没有中文 弃
-// async function queryIpApi(proxy) {
-//   return new Promise((resolve, reject) => {
-//     const url = `http://v4.ident.me/json`;
-//     let node = ProxyUtils.produce([proxy], target);
-//     const timeoutPromise = new Promise((_, reject) => {
-//       setTimeout(() => {
-//         reject(new Error("请求超时,丢弃节点"));
-//       }, timeout);
-//     });
-//     const queryPromise = $.http.get({ url, node: node, "policy-descriptor": node, }) .then((resp) => {
-//         const data = JSON.parse(resp.body);
-//         if (data.tz) {
-//           resolve(data);
-//         } else {
-//           reject(new Error(data.message));
-//         }
-//       })
-//       .catch((err) => {
-//         console.log("api = " + err);
-//         reject(err);
-//       });
-//     // 超时处理
-//     Promise.race([timeoutPromise, queryPromise]).catch((err) => {
-//       reject(err);
-//     });
-//   });
-// }
-
 // 查询落地ip
 async function queryIpApi(proxy) {
   return new Promise((resolve, reject) => {
     const url = `http://ip-api.com/json?lang=zh-CN&fields=status,message,country,countryCode,city,query`;
     let node = ProxyUtils.produce([proxy], target);
-
     const timeoutPromise = new Promise((_, reject) => {
       setTimeout(() => {
         reject(new Error("请求超时,丢弃节点"));
       }, timeout);
     });
-
-    const queryPromise = $.http
-      .get({
-        url,
-        // opts: opts, // QX的写法
-        node: node, // Loon和Surge IOS
+    const queryPromise = $.http.get({url, node: node, // Loon or Surge IOS 
         "policy-descriptor": node, // Surge MAC
-      })
-      .then((resp) => {
+      }).then((resp) => {
         const data = JSON.parse(resp.body);
         if (data.status === "success") {
           resolve(data);
         } else {
           reject(new Error(data.message));
-        }
-      })
-      .catch((err) => {
-        console.log("api = " + err);
-        reject(err);
-      });
+        } }).catch((err) => { console.log("api = " + err); reject(err); });
     // 超时处理
-    Promise.race([timeoutPromise, queryPromise]).catch((err) => {
-      reject(err);
-    });
+    Promise.race([timeoutPromise, queryPromise]).catch((err) => { reject(err); });
   });
 }
-
-function removeDuplicateName(arr) {
-  const nameSet = new Set();
-  const result = [];
-  for (const e of arr) {
-    if (e.qc && !nameSet.has(e.qc)) {
-      nameSet.add(e.qc);
-      result.push(e);
-    }
-  }
-  return result;
-}
-function removeqcName(arr) {
-  const nameSet = new Set();
-  const result = [];
-  for (const e of arr) {
-    if (!nameSet.has(e.qc)) {
-      nameSet.add(e.qc);
-      const modifiedE = { ...e };
-      delete modifiedE.qc;
-      result.push(modifiedE);
-    }
-  }
-  return result;
-}
-function processProxies(proxies) {
-  const groupedProxies = proxies.reduce((groups, item) => {
-    const existingGroup = groups.find((group) => group.name === item.name);
-    if (existingGroup) {
-      existingGroup.count++;
-      existingGroup.items.push({
-        ...item,
-        name: `${item.name} ${existingGroup.count.toString().padStart(2, "0")}`,
-      });
-    } else {
-      groups.push({
-        name: item.name,
-        count: 1,
-        items: [{ ...item, name: `${item.name} 01` }],
-      });
-    }
-    return groups;
-  }, []);
-  const sortedProxies = groupedProxies.flatMap((group) => group.items);
-  proxies.splice(0, proxies.length, ...sortedProxies);
-  return proxies;
-}
-function getFlagEmoji(cc) {
-  const codePoints = cc
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints).replace(/🇹🇼/g, "🇨🇳");
-}
+function removeDuplicateName(arr){const nameSet=new Set;const result=[];for(const e of arr){if(e.qc&&!nameSet.has(e.qc)){nameSet.add(e.qc);result.push(e)}}return result}
+function removeqcName(arr){const nameSet=new Set;const result=[];for(const e of arr){if(!nameSet.has(e.qc)){nameSet.add(e.qc);const modifiedE={...e};delete modifiedE.qc;result.push(modifiedE)}}return result}
+function processProxies(proxies) {const groupedProxies = proxies.reduce((groups, item) => {const existingGroup = groups.find(group => group.name === item.name);if (existingGroup) {existingGroup.count++;existingGroup.items.push({ ...item, name: `${item.name} ${existingGroup.count.toString().padStart(2, '0')}` });} else {groups.push({ name: item.name, count: 1, items: [{ ...item, name: `${item.name} 01` }] });}return groups;}, []);const sortedProxies = groupedProxies.flatMap(group =>group.items);proxies.splice(0,proxies.length, ...sortedProxies);return proxies;}
+function getFlagEmoji(cc){const codePoints=cc.toUpperCase().split("").map((char=>127397+char.charCodeAt()));return String.fromCodePoint(...codePoints).replace(/🇹🇼/g,"🇨🇳")}
